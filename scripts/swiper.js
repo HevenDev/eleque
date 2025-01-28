793388ac93b8c3d5f8eefbe987ef81682cf39ccf
HevenDev <hevendevofficial@gmail.com>
2025-01-28 13:06:54 +0530

    Changes in CSS and Add contact section

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
   $('.slider').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
-      arrows: true,
       dots: true,
       autoplay: true,
       autoplaySpeed: 2000,
       centerMode: true, // Enable center mode
-      centerPadding: '10%', // Adjust this value for the gap
+      centerPadding: '10%', 
   });
 });
